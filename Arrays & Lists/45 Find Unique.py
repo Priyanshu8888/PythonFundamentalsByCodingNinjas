7826e6eff0ff6645a33cbb04d0bc1dfe644b7cbe
Priyanshu8888 <priyanshu.karmakar.fiem.ece19@teamfuture.in>
2022-03-09 16:59:10 +0530

    Initial commit

---
+++ Arrays & Lists/45 Find Unique.py
@@ -0,0 +1,32 @@
+import sys
+
+def findUnique(arr, n) :
+    #Your code goes here
+    res = arr[0]
+     
+    # Do XOR of all elements and return
+    for i in range(1,n):
+        res = res ^ arr[i]
+     
+    return res
+    
+#Taking Input Using Fast I/O
+def takeInput() :
+    n = int(sys.stdin.readline().rstrip())
+
+    if n == 0 :
+        return list(), 0
+
+    arr = list(map(int, sys.stdin.readline().rstrip().split(" ")))
+    return arr, n
+
+
+#main
+t = int(sys.stdin.readline().rstrip())
+
+while t > 0 :
+
+    arr, n = takeInput()
+    print(findUnique(arr, n))
+
+    t -= 1
